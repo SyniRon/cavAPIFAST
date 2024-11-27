fe0cddb6b6dfcf44f192e92e0670bd275536dbe2
SyniRon <66834451+SyniRon@users.noreply.github.com>
2024-11-26 23:29:16 -0500

    correct references

--- app/services/username.py
+++ app/services/username.py
@@ -2,4 +2,4 @@ from sqlmodel import Session, select
 from app.models import user_models
 
 def get_user_by_username(session: Session, username: str):
-    return session.exec(select(xf_user).where(xf_user.username == username)).first()
+    return session.exec(select(user_models.xf_user).where(user_models.xf_user.username == username)).first()
