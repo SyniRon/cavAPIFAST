47957c152867ad5d73aa3fbf63815c9e0dfdbe94
SyniRon <66834451+SyniRon@users.noreply.github.com>
2024-12-10 23:55:26 -0500

    remove debug prints

--- app/services/query_rosters.py
+++ app/services/query_rosters.py
@@ -6,13 +6,9 @@ def get_roster_by_user_id(session: Session, user_id: int):
 
 def get_field_value_by_relation_id(session: Session, relation_id: int):
     results = session.exec(select(xenforo_models.XfNfRostersFieldValue).where(xenforo_models.XfNfRostersFieldValue.relation_id == relation_id)).all()
-    print(results)
     field_values = {result.field_id.decode('utf-8'): result.field_value for result in results}
-    print(field_values)
     join_date = field_values.get('joinDate', "")
     promo_date = field_values.get('promoDate', "")
-    print(join_date)
-    print(promo_date)
     return join_date, promo_date
 
 def get_service_record_by_relation_id(session: Session, relation_id: int):
