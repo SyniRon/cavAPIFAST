27a5df9a1c4fd8f1d6c1041faa143184b5674d0a
SyniRon <66834451+SyniRon@users.noreply.github.com>
2024-12-10 23:49:23 -0500

    Error handling for non-member users
    Returns 404 for when a user id is valid but the user isn't found in any cav rosters

--- app/api/v1/userid.py
+++ app/api/v1/userid.py
@@ -17,10 +17,12 @@ router = APIRouter()
 async def get_by_id(user_id: int, session: Session = Depends(get_session)):
     user = get_user_by_id(session, user_id)
     if not user:
-        raise HTTPException(404, "Username not found")
+        raise HTTPException(404, "User id not found")
     roster = get_roster_by_user_id(session, user_id)
+    if not roster:
+        raise HTTPException(404, "User found but not associated with any rosters")
     join_date, promo_date = get_field_value_by_relation_id(session, roster.relation_id)
     service_record = get_service_record_by_relation_id(session, roster.relation_id)
     awards = get_awards_by_relation_id(session, roster.relation_id)
     # TODO: build out return to match response model
-    return {"joinDate": join_date, "promoDate": promo_date, "serviceRecord": service_record, "awards": awards}
+    return {"username": user.username, "joinDate": join_date, "promoDate": promo_date, "serviceRecord": service_record, "awards": awards}
